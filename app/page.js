514d0d77689d6ea76f608c2a74a4fc79ac7d6bef
Mohamed Riyas A. M. <5379404+mriyasam@users.noreply.github.com>
2026-01-15 18:06:30 -0700

    PIN updated

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function GrocerySearch() {
   const [compareResults, setCompareResults] = useState([]);
   const [message, setMessage] = useState({ text: "", type: "" });
 
-  const ADMIN_PIN = "1234";
+  const ADMIN_PIN = "3044";
 
   // --- LOGIC: SEARCH ---
   const fetchPrices = useCallback(
